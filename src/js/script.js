aae1dbaac178c0164ba0d891671fc75daa966fc7
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-08-18 17:10:23 +0700

    adaptive slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		// adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png" alt="slick"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png" alt="slick"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="slick"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="slick"></button>',
 		responsive: [
 			{
 				breakpoint: 992,
