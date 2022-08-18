adbe224ceef48408586e8dc6027477dabd10b2ba
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-08-18 16:56:56 +0700

    add main slider

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		// adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png" alt="slick"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png" alt="slick"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+});
