674ff481314ea512ffc3665de0af2b27dd793019
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-08-26 12:15:06 +0700

    check site on validator and add animations for section feedbacks

--- src/js/script.js
+++ src/js/script.js
@@ -128,5 +128,7 @@ $(document).ready(function () {
 		var _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
-	})
+	});
+	
+	new WOW().init();
 }); 
